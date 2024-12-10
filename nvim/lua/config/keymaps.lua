bb553c586b478beaf143af45337b380827d2543a
JB Ferguson <jb_ferguson@cargill.com>
2024-12-10 07:59:29 -0600

    Add new maps and tweaks.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,3 +2,11 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode" })
+
+vim.keymap.set("n", "<leader>p", '<cmd>lua require("cmp").setup { enabled = true }<cr>', { desc = "Enable completion" })
+vim.keymap.set(
+  "n",
+  "<leader>P",
+  '<cmd>lua require("cmp").setup { enabled = false }<cr>',
+  { desc = "Disable completion" }
+)
